cbf67fe293e1d8d306283f0909495ac856fd9585
Alibek Birlikbai <alibekbirlikbai21@gmail.com>
2024-03-14 17:49:55 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get('/get-file-content', async (req, res) => {
+  try {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id=1FJ6-cxOf7RflfDXCjfM99RuPLPp8anT0';
+    const response = await fetch(fileUrl);
+    const data = await response.text();
+
+    // Github API accessToken
+    res.send(data);
+  } catch (error) {
+    console.error('Ошибка при чтении файла:', error);
+    res.status(500).send('Ошибка при чтении файла');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Прокси-сервер запущен на порту ${PORT}`);
+});
